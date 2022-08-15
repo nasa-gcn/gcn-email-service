1e1ec2cc0e6e9b110ab36858fe6c874d4727ba82
dakota002 <dakota.d@live.com>
2022-08-15 10:50:54 -0400

    remove commented out section

--- gcn_email_service/core.py
+++ gcn_email_service/core.py
@@ -1,13 +1,3 @@
-# import botocore 
-# import botocore.session 
-# from aws_secretsmanager_caching import SecretCache, SecretCacheConfig 
-
-# client = botocore.session.get_session().create_client('secretsmanager')
-# cache_config = SecretCacheConfig()
-# cache = SecretCache( config = cache_config, client = client)
-
-# secret = cache.get_secret_string('mysecret')
-
 import boto3
 import base64
 from botocore.exceptions import ClientError
