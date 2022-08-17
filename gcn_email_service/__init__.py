fcebd926f260bfaea1e5ad18463d9e2f696b924d
dakota002 <dakota.d@live.com>
2022-08-17 15:12:05 -0400

    Fixing line-too-long issue

--- gcn_email_service/__init__.py
+++ gcn_email_service/__init__.py
@@ -19,7 +19,9 @@ from gcn_kafka import Consumer, config_from_env
 from ratelimit import limits, RateLimitException
 from backoff import on_exception, expo
 
-SENDER = "GCN Alerts <{}>".format(config_from_env(prefix='EMAIL_')['EMAIL_SENDER'])
+SENDER = "GCN Alerts <{}>".format(
+    config_from_env(prefix='EMAIL_')['EMAIL_SENDER']
+)
 CHARSET = "UTF-8"
 SUBJECT = "GCN/{}"
 # Used for testing attachment sends, works for a local file
