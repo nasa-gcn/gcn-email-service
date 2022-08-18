eba9e68d89012a1ac6938944bd4e080424178de1
dakota002 <dakota.d@live.com>
2022-08-18 09:45:20 -0400

    White space poetry fix

--- gcn_email_service/helpers.py
+++ gcn_email_service/helpers.py
@@ -5,6 +5,7 @@ import threading
 # Adapted from https://gist.github.com/Depado/7925679
 logger = logging.getLogger(__name__)
 
+
 def periodic_task(interval):
     def outer_wrap(function):
         def wrap(*args, **kwargs):
