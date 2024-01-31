6da580826b50f7c8dd04b8a49dc77f5c6d37fd92
Leo Singer <leo.singer@ligo.org>
2024-01-31 10:13:35 -0500

    Configure black formatter

--- gcn_email/core.py
+++ gcn_email/core.py
@@ -18,21 +18,22 @@ import backoff
 from .helpers import periodic_task
 from . import metrics
 
-SESV2 = boto3.client('sesv2')
+SESV2 = boto3.client("sesv2")
 SENDER = f'GCN Notices <{os.environ["EMAIL_SENDER"]}>'
 
 # Maximum send rate
-MAX_SENDS = boto3.client('ses').get_send_quota()['MaxSendRate']
+MAX_SENDS = boto3.client("ses").get_send_quota()["MaxSendRate"]
 
 log = logging.getLogger(__name__)
 
 
 def get_email_notification_subscription_table():
-    client = boto3.client('ssm')
+    client = boto3.client("ssm")
     result = client.get_parameter(
-        Name='/RemixGcnProduction/tables/email_notification_subscription')
-    table_name = result['Parameter']['Value']
-    return boto3.resource('dynamodb').Table(table_name)
+        Name="/RemixGcnProduction/tables/email_notification_subscription"
+    )
+    table_name = result["Parameter"]["Value"]
+    return boto3.resource("dynamodb").Table(table_name)
 
 
 def query_and_project_subscribers(table, topic):
@@ -43,20 +44,21 @@ def query_and_project_subscribers(table, topic):
     """
     try:
         response = table.query(
-            IndexName='byTopic',
+            IndexName="byTopic",
             ProjectionExpression="#topic, recipient",
             ExpressionAttributeNames={"#topic": "topic"},
-            KeyConditionExpression=(Key('topic').eq(topic)))
+            KeyConditionExpression=(Key("topic").eq(topic)),
+        )
     except Exception:
-        log.exception('Failed to query recipients')
+        log.exception("Failed to query recipients")
         return []
     else:
-        return [x['recipient'] for x in response['Items']]
+        return [x["recipient"] for x in response["Items"]]
 
 
 def connect_as_consumer():
-    log.info('Connecting to Kafka')
-    return Consumer(config_from_env(), **{'enable.auto.commit': False})
+    log.info("Connecting to Kafka")
+    return Consumer(config_from_env(), **{"enable.auto.commit": False})
 
 
 @periodic_task(86400)
@@ -65,26 +67,32 @@ def subscribe_to_topics(consumer: Consumer):
     # This may need to be updated if new topics have a format different than
     # 'gcn.classic.[text | voevent | binary].[topic]'
     topics = [
-        topic for topic in consumer.list_topics().topics
-        if topic.startswith('gcn.')]
-    log.info('Subscribing to topics: %r', topics)
+        topic for topic in consumer.list_topics().topics if topic.startswith("gcn.")
+    ]
+    log.info("Subscribing to topics: %r", topics)
     consumer.subscribe(topics)
 
 
 def kafka_message_to_email(message):
     topic = message.topic()
     email_message = EmailMessage()
-    if topic.startswith('gcn.classic.text.'):
+    if topic.startswith("gcn.classic.text."):
         email_message.set_content(message.value().decode())
-    elif topic.startswith('gcn.classic.voevent.'):
+    elif topic.startswith("gcn.classic.voevent."):
         email_message.add_attachment(
-            message.value(), filename='notice.xml',
-            maintype='application', subtype='xml')
+            message.value(),
+            filename="notice.xml",
+            maintype="application",
+            subtype="xml",
+        )
     else:
         email_message.add_attachment(
-            message.value(), filename='notice.bin',
-            maintype='application', subtype='octet-stream')
-    email_message['Subject'] = topic
+            message.value(),
+            filename="notice.bin",
+            maintype="application",
+            subtype="octet-stream",
+        )
+    email_message["Subject"] = topic
     return email_message.as_bytes()
 
 
@@ -94,19 +102,19 @@ def recieve_alerts(consumer):
         for message in consumer.consume(timeout=1):
             error = message.error()
             if error is not None:
-                log.error('Error consuming message: %s', error)
+                log.error("Error consuming message: %s", error)
                 continue
             topic = message.topic()
             metrics.received.labels(topic).inc()
             recipients = query_and_project_subscribers(table, topic)
             if recipients:
-                log.info('Sending message for topic %s', topic)
+                log.info("Sending message for topic %s", topic)
                 email = kafka_message_to_email(message)
                 for recipient in recipients:
                     try:
                         send_raw_ses_message_to_recipient(email, recipient)
                     except Exception:
-                        log.exception('Failed to send message')
+                        log.exception("Failed to send message")
                     else:
                         metrics.sent.labels(topic, recipient).inc()
             consumer.commit(message)
@@ -123,12 +131,13 @@ def recieve_alerts(consumer):
         SESV2.exceptions.SendingPausedException,
         SESV2.exceptions.TooManyRequestsException,
     ),
-    max_time=300
+    max_time=300,
 )
 @limits(calls=MAX_SENDS, period=1)
 @metrics.send_request_latency_seconds.time()
 def send_raw_ses_message_to_recipient(bytes, recipient):
     SESV2.send_email(
         FromEmailAddress=SENDER,
-        Destination={'ToAddresses': [recipient]},
-        Content={'Raw': {'Data': bytes}})
+        Destination={"ToAddresses": [recipient]},
+        Content={"Raw": {"Data": bytes}},
+    )
--- gcn_email/metrics.py
+++ gcn_email/metrics.py
@@ -9,20 +9,17 @@
 import prometheus_client
 
 received = prometheus_client.Counter(
-    'received',
-    'Kafka messages received',
-    labelnames=['topic'],
-    namespace=__package__)
+    "received", "Kafka messages received", labelnames=["topic"], namespace=__package__
+)
 
 
 sent = prometheus_client.Counter(
-    'sent',
-    'Emails sent',
-    labelnames=['topic', 'address'],
-    namespace=__package__)
+    "sent", "Emails sent", labelnames=["topic", "address"], namespace=__package__
+)
 
 
 send_request_latency_seconds = prometheus_client.Histogram(
-    'send_request_latency_seconds',
-    'Time taken to send each message',
-    namespace=__package__)
+    "send_request_latency_seconds",
+    "Time taken to send each message",
+    namespace=__package__,
+)
